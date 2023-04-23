2e08f820d20e10859541b653e3eca162370b0a04
Jocokiss <joco.kiss.n@gmail.com>
2023-04-23 10:19:08 +0200

    rearranged and structured

--- microblog.py
+++ microblog.py
@@ -1,5 +1,9 @@
-from application import app, db, cli
-from application.models import User, Post
+from app import create_app, db, cli
+from app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
 
 @app.shell_context_processor
 def make_shell_context():
